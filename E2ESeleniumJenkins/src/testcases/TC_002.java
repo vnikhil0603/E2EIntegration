44b7a331fd0eeb0ec2b6d98dab46980a84508477
Nikil <Nikil@Nikil-PC>
2018-08-22 14:01:00 +0530

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC_002.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("nikhil");
+		driver.findElement(By.id("pass")).sendKeys("123345");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		driver.quit();
+		
+	}
+
+}
